f49a8df27c179eed03c35483ce366dc34022da22
edickins <bleepbloop@bleepbloop.net>
2021-09-09 23:31:59 +0100

    tidy up

--- project/index.js
+++ project/index.js
@@ -10,35 +10,41 @@ async function getLibraryAsJson(path) {
   return library;
 }
 
-async function getPlaylists(library) {
+function getPlaylists(library) {
   return library.Playlists;
 }
 
-function getPlaylistData(playlistObj, tracks) {
+function getPlaylistData(playlistObj, allTracks) {
   const items = playlistObj["Playlist Items"] || [];
 
+  // return Array of playlist track data
   return items.map((value) => {
-    return getTrackData(value, tracks);
+    return getTrackData(value, allTracks);
   });
 }
 
-function getTrackData(trackObj, tracks) {
-  const track = tracks[trackObj["Track ID"]];
+function getTrackData(trackObj, allTracks) {
+  const track = allTracks[trackObj["Track ID"]];
+  //todo: format track data appropriately
   /* for (key in track) {
     if (key.indexOf(" ") > -1) {
       console.log(`key ${key} contains a space`);
     }
   } */
-  //console.log(`${track["Name"]} by ${track["Artist"]}`);
-  return track;
+
+  if (track) {
+    console.log(`${track["Name"]} by ${track["Artist"]}`);
+    return track;
+  }
 }
 
 async function run() {
   const libraryObj = await getLibraryAsJson("./data/iTunesLibrary.xml");
-  const playlists = await getPlaylists(libraryObj);
+  const playlists = getPlaylists(libraryObj);
 
   playlists.forEach((playlistObj) => {
-    const tracks = getPlaylistData(playlistObj, libraryObj.Tracks);
+    // todo: use playlistTracks
+    const playlistTracks = getPlaylistData(playlistObj, libraryObj.Tracks);
     console.log(playlistObj);
   });
   console.log(`there are ${playlists.length} playlists`);
